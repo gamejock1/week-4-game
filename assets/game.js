d2faa62f786e953cb1e892425d40fcacb71c7747
John yeager <johnyeager@lawn-128-61-34-149.lawn.gatech.edu>
2018-03-08 20:29:54 -0500

    final draft

--- assets/game.js
+++ assets/game.js
@@ -1,40 +1,68 @@
 
 
-// $(document).ready(function(){
+$(document).ready(function(){
 
 var wins = 0;
 var losses = 0;
 var targetNumber = 0;
 var userGuess = 0;
-var jewel1 = 0;
-var jewel2 = 0;
-var jewel3 = 0;
-var jewel4 = 0;
+var jewel1 = $("#gem");
+var jewel2 = $("#gem1");
+var jewel3 = $("#gem2");
+var jewel4 = $("#gem3");
+var userguessdiv = $("#yournumber")
+var gemcounter = 0;
+var targetnumber = 0;
 
-function jewelValues(){
-jewel1 = Math.floor((Math.random() * 12) + 1);
-jewel2 = Math.floor((Math.random() * 12) + 1);
-jewel3 = Math.floor((Math.random() * 12) + 1);
-jewel4 = Math.floor((Math.random() * 12) + 1);
+
+$("#gem" ).attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem1").attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem2").attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem3").attr("value", (Math.floor((Math.random() * 12) + 1)));
+
+var targetnumber = ("value", (Math.floor((Math.random() * 101) + 19)));
+$("#targetnumber").text(targetnumber)
+
+
+
+$(".gem").on("click", function(){
+var gemvalue = ($(this).attr("value"));
+gemvalue = parseInt(gemvalue);
+gemcounter += gemvalue;
+userguessdiv.text(gemcounter);
+console.log(gemvalue)
+
+if (targetnumber === gemcounter){
+++wins;
+$("#wins").text(wins)
+reset();
 }
 
-// console.log(jewelValues())
-
-function main (){
-  jewelValues()
-  console.log(
-    jewel1 + jewel2 + jewel3 + jewel4
-  )
+if (gemcounter > targetnumber){
+++losses;
+$("#losses").text(losses);
+reset()
 }
 
+});
 
 
+function reset(){
+userGuess = 0;
+gemcounter = 0;
 
 
+$("#gem" ).attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem1").attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem2").attr("value", (Math.floor((Math.random() * 12) + 1)));
+$("#gem3").attr("value", (Math.floor((Math.random() * 12) + 1)));
 
+targetnumber = ("value", (Math.floor((Math.random() * 101) + 19)));
+$("#targetnumber").text(targetnumber)
 
-
-// });
+userguessdiv.text(gemcounter);
+}
+});
 
 
 
